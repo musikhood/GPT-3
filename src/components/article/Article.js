3a457202d10b7fa9fbbd7f3f4fa900b00a52bdb9
Rafał Białowski <b.bialowski@gmail.com>
2021-12-30 14:06:02 +0100

    Add Blog

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './article.sass';
 
-export default function Article() {
+export default function Article({imgUrl,date,title}) {
   return (
-    <div>
-      Article
+    <div className='gpt3__blog-container-article'>
+      <div className='gpt3__blog-container-article-image'>
+        <img src={imgUrl} alt="blog image"/>
+      </div>
+      <div className='gpt3__blog-container-article-content'>
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
     </div>
   );
 }
