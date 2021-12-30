2e0bfa19a028da5d8a503c5c0ff608c81a0714eb
Rafał Białowski <b.bialowski@gmail.com>
2021-12-30 12:12:19 +0100

    Add whatGPT3

--- src/components/feature/Feature.js
+++ src/components/feature/Feature.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './feature.sass';
 
-export default function Feature() {
+export default function Feature({title,text}) {
   return (
-    <div>
-      Feature
+    <div className='gpt3__features-container-feature'>
+      <div className='gpt3__features-container-feature-title'>
+        <div></div>
+        <h1>{title}</h1>
+      </div>
+      <div className='gpt3__features-container-feature-text'>
+        <p>{text}</p>
+      </div>
     </div>
   );
 }
