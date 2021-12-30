0efbd89bdfd37b082e9adf0c76464e3abb94a1e5
Rafał Białowski <b.bialowski@gmail.com>
2021-12-30 13:23:04 +0100

    Add cta

--- src/containers/features/Features.js
+++ src/containers/features/Features.js
@@ -4,18 +4,22 @@ import './features.sass';
 
 const featuresData = [
   {
+    id: 1,
     title: 'Improving end distrusts instantly ',
     text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.'
   },
   {
+    id: 2,
     title: 'Become the tended active',
     text: 'Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to.'
   },
   {
+    id: 3,
     title: 'Message or am nothing',
     text: 'Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address.'
   },
   {
+    id: 4,
     title: 'Really boy law county',
     text: 'Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush.'
   },
@@ -34,7 +38,7 @@ export default function Features() {
         </p>
       </div>
       <div className='gpt3__features-container'>
-        {featuresData.map((item,index)=><Feature key={index} title={item.title} text={item.text}/>)}
+        {featuresData.map((item)=><Feature key={item.id} title={item.title} text={item.text}/>)}
       </div>
     </div>
   );
